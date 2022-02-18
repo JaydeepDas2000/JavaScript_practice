146d4fe1aa97668a84808fb820299688de422f13
JaydeepDas <jaydeep@gmail.com>
2022-02-18 21:25:37 +0530

    Start learning JS from ATO

---
+++ JavaScriptATO/Lesson_2/app.js
@@ -0,0 +1,11 @@
+// For singleline commment
+/* 
+For
+Murltiline
+Comments
+*/
+
+let x = 7;  // let is used for creating variable
+let y = 3;
+let z = x + y;
+console.log('Answer: ' + z);
---
+++ JavaScriptATO/Lesson_3/variables.js
@@ -0,0 +1,12 @@
+var x = 7; // var used for variable but in latest release recommendation is to abandon var.
+// try to used let and const keyword
+let y = 3;
+const z = x + y; // const is another keyword help to declaration. we used it when we don't
+// want to change its value or become a constant
+console.log('Answer: ' + z);
+
+
+// why we should not use var keyword
+/*
+    we can use var keyword before declaration. which is aganst scope
+*/
