65f74eec5dd1db17806867999e6a03f91ffc90ee
JaydeepDas <jaydeep@gmail.com>
2022-02-19 17:04:24 +0530

    dataType

---
+++ Calculator/script.js
@@ -0,0 +1,5 @@
+let x,y,result;
+x = document.getElementById('firstNumber');
+y = document.getElementById('secondNumber');
+
+
--- JavaScriptATO/Lesson_4/dataType.js
+++ JavaScriptATO/Lesson_4/dataType.js
@@ -1,1 +1,17 @@
 // typeof keyword is use to know the data type of that varibles
+
+let x = 7;
+console.log(typeof x); // It will show 'number'
+
+let y = true;
+console.log(typeof y); // it will show 'boolean'
+
+let z = 'Hello World';
+console.log(typeof z); // it will show 'string'
+
+let a;
+console.log(a); // it will show 'undefined' value
+console.log(typeof a); // it will also show 'undefined' datatype
+
+
+/* this are the basic data type in JS. */
