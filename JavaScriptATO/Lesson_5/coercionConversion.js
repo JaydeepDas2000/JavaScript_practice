54065892a1823b86723aab75568c129f7a6dd575
JaydeepDas <jaydeep@gmail.com>
2022-02-19 17:23:43 +0530

    Conversion

--- JavaScriptATO/Lesson_5/coercionConversion.js
+++ JavaScriptATO/Lesson_5/coercionConversion.js
@@ -1,4 +1,4 @@
-// 6 type of coercion and conversion
+// type of coercion and conversion
 
 let a = 7; // this is a number
 let b = '6'; // this is a string
@@ -11,3 +11,11 @@ b = parseInt(b); // we convert string to int
 
 c = a + b; // now here it will add with each other because both are in number datatype
 console.log('New Answer: ' + c);
+
+// now we try to pass a string which is not a digit characters.
+
+let d = parseInt("not a number");
+console.log(d); // it will show NaN. this is not a error, it just tell that this is not a number
+
+let e = isNaN(d); // isNaN() function help to identify that varible which we passed in it is not a number or not
+console.log(e); // it will show True.
