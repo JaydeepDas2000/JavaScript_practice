92b87c5972a2cbc6bca21f64243b83bba76d17bb
JaydeepDas2000 <jaydeep.das@outlook.in>
2022-02-19 21:43:02 +0530

    Function2

--- JavaScriptATO/Lesson_8/functionDeclaration.js
+++ JavaScriptATO/Lesson_8/functionDeclaration.js
@@ -9,7 +9,7 @@ sayHello();  // function call
 let a = sayHello; // here we don't use function invoke operation when we want to assign inside any variable
 a(); // when every we want to call any function then we used function invoke 
 
-console.log("New Function")
+console.log("1 Function");
 
 function showName(name){
     console.log("Hello " + name + " !");
@@ -18,3 +18,13 @@ function showName(name){
 showName('Jaydeep');
 showName('Ankit');
 showName('Rohan');
+
+console.log("2 function");
+
+function calculateTax(amount){
+    let result = amount*0.18;
+    return result;
+}
+
+let tax = calculateTax(100);
+console.log(tax);
