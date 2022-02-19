f160be1d4a5a17dab5612245262f0c39854f402b
JaydeepDas <jaydeep@gmail.com>
2022-02-19 18:09:23 +0530

    expressions and operators

---
+++ JavaScriptATO/Lesson_6/expressions.js
@@ -0,0 +1,17 @@
+// Expressions and Operations
+
+// Types of expressions
+
+// 1. Variables Declaration
+// let a;
+
+// 2. Assign a value
+// a = 4;
+
+// 3. Evalution that returns a single value
+// b + c
+
+let b = 3;
+let c = 2;
+// Three expressions in here !
+let a = b + c;
---
+++ JavaScriptATO/Lesson_6/operators.js
@@ -0,0 +1,22 @@
+// Categories of Operators
+
+// Assignment =
+// Arithnetic + - * / %
+// Increment / Decrement ++ --
+
+// string '' + 
+// Precedence ()
+/*
+    var b = (1 + 2) * 3;
+    console.log(b) // it will show 9.
+*/
+
+// function invocation operators -> using this ' () ' in function
+
+// Logical and: && or: ||
+
+// Member accessor operator -> using this ' . ' to call method
+
+// Code block operators {...}
+
+// Array element access operators [...]
