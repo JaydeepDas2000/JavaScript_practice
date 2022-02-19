b6f6e71fcfe1e39e7485d38cd24e88f0a95d7f83
JaydeepDas <jaydeep@gmail.com>
2022-02-19 17:11:01 +0530

    coercion

---
+++ JavaScriptATO/Lesson_5/coercionConversion.js
@@ -0,0 +1,8 @@
+// 6 type of coercion and conversion
+
+let a = 7; // this is a number
+let b = '6'; // this is a string
+
+let c = a + b; // due to number and string. it will concatenate 
+
+console.log('Answer: ' + c);
