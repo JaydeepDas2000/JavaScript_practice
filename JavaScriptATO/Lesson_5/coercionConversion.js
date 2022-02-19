277343e78667e34a5be4984802616b08799a777b
JaydeepDas <jaydeep@gmail.com>
2022-02-19 17:15:33 +0530

    update coercion

--- JavaScriptATO/Lesson_5/coercionConversion.js
+++ JavaScriptATO/Lesson_5/coercionConversion.js
@@ -6,3 +6,8 @@ let b = '6'; // this is a string
 let c = a + b; // due to number and string. it will concatenate 
 
 console.log('Answer: ' + c);
+
+b = parseInt(b); // we convert string to int
+
+c = a + b; // now here it will add with each other because both are in number datatype
+console.log('New Answer: ' + c);
