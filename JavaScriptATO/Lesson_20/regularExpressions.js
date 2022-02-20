2ac69fa29fcd272d0705821e5c79001f614ab712
JaydeepDas2000 <jaydeep.das@outlook.in>
2022-02-20 16:19:05 +0530

    regular expressions

---
+++ JavaScriptATO/Lesson_20/regularExpressions.js
@@ -0,0 +1,12 @@
+let pattern = /xyz/;
+
+console.log(pattern);
+console.log(typeof pattern); // object datatype
+
+let value = 'this is xyz a test.';
+
+console.log(pattern.test(value)); // it will help to say that pattern is present in that value string
+
+console.log(value.replace(pattern, 'just')); // it will help to replace word from another word
+
+console.log(value.match(pattern)); // it will help to display description
