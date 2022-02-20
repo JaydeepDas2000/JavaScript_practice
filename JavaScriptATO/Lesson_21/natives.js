16918238b7a15f2868b2d11fef5e682c7730bb33
JaydeepDas2000 <jaydeep.das@outlook.in>
2022-02-20 16:39:53 +0530

    natives

---
+++ JavaScriptATO/Lesson_21/natives.js
@@ -0,0 +1,33 @@
+/* let pattern = /xyz/;
+let value = 'This is xyz a test.';
+console.log(value.replace(pattern, 'just')); // here replace is a method which is already define
+ */
+
+let myString = new String('howdy');
+console.log(myString);
+console.log(typeof myString);
+console.log(myString.toString()); // convert native string function back into a primitive sting
+console.log(typeof myString.toString());
+
+/*
+    String()
+    Number()
+    Boolean()
+    Object()
+    Function()
+    Symbol()
+*/
+
+/*
+    Array()
+    RegExp()
+*/
+
+/*
+    Date()
+    Error()
+*/
+
+let myPrimitive = 'THIS IS CRAZY';
+myPrimitive = myPrimitive.toLowerCase();
+console.log(myPrimitive)
