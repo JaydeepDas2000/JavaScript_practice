b3a06cc6b487cd7824df9717dd51573eafa3b781
JaydeepDas2000 <jaydeep.das@outlook.in>
2022-02-20 18:02:30 +0530

    truthy falsy

---
+++ JavaScriptATO/Lesson_26/truthyFalsy.js
@@ -0,0 +1,23 @@
+/*
+    if(false) // false is falsy
+    if(null) // null is falsy
+    if(undefined) // undefined is falsy
+    if(0) // 0 is falsy
+    if(NaN) // NaN is falsy
+    if('') // an empty string with single-quotes is falsy
+    if("") // an empty string with double-quotes is falsy
+*/
+
+/*
+    if(true) // true is truthy
+    if({}) // an empty object is truthy
+    if([]) // an empty array is truthy
+    if('bob') // a non-empty sting is truthy
+    if(new RegExp()) // a new instance of an object is truthy
+    if(10) // +ve int are truthy
+    if(-10) // -ve int are truthy
+    if(1.23) // +ve float are truthy
+    if(-1.23) // -ve float are truthy
+    if(Infinity) // +ve infinity is truthy
+    if(-Infinity) // -ve infinity is truthy
+*/
